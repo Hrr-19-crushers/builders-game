dfb4e853e2f6d538123d064133468946a8878e65
David Bisrat <dabisrat@gmail.com>
2016-11-11 15:17:53 -0500

    update files

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,27 @@
-//example
+// example
 
-const counter: number = 42;
-const isDone: boolean = false;
-//const hello: string = 454; // should raise and error on compile
+// const counter: number = 42;
+// const isDone: boolean = false;
+// const hello: string = 454; // should raise and error on compile
+
+ import * as express from 'express';
+ import * as http from 'http';
+ import * as socket_io from 'socket.io';
+// const express = require('express');
+//const http = require('http');
+// const sockt_io = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socket_io.listen(server);
+
+app.use(express.static(__dirname + '../../build'));
+server.listen(3000);
+console.log('connected listening on port 3000');
+
+app.get('/', (req, res) => res.sendFile('index'));
+// when a client connects this fires
+io.on('connection', (socket) => {
+  socket.emit('serverResponse', 'hello new player');
+});
+
---
+++ type-declarations/http.d.ts
@@ -0,0 +1,3 @@
+// declare var http: {
+//   Server: any
+// };

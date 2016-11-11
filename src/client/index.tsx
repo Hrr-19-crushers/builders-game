9dd273b838689f1b12c2eac8c966608489c9f0ab
jonathan.deng <dengjonathan@gmail.com>
2016-11-11 17:58:05 -0500

    got server side type decs working

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,4 +1,3 @@
-/// <reference path="../../type-declarations/require.d.ts" />
 import * as React from 'react'; //this is the babel way
 import { render } from 'react-dom';
 
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,8 @@
 //example
+import * as http from 'http';
 
+const server = http.createServer();
+server.listen(8080)
 const counter: number = 42;
 const isDone: boolean = false;
 //const hello: string = 454; // should raise and error on compile

51dd826f72cf4d32ea543d2f72c631541950be4d
David Bisrat <dabisrat@gmail.com>
2016-11-11 17:58:11 -0500

    setting up socketio

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,7 +4,8 @@ import { render } from 'react-dom';
 
 import HelloWorld from './components/App';
 import AnotherComp from './components/AnotherComponent';
-
+import sio from './socket_io';
+import Chat from './components/Chat';
 const forty: number = 42;
 
 // using require to import npm module without type declarations
@@ -18,20 +19,20 @@ const rootEl = document.getElementById('root');
 //And render our App into it, inside the HMR App ontainer which handles the hot reloading
 render(
   <AppContainer>
-    <HelloWorld />
+    <Chat />
   </AppContainer>,
   rootEl
 );
 
 // Handle hot reloading requests from Webpack
 if (module.hot) {
-  module.hot.accept('./components/App', () => {
+  module.hot.accept('./components/Chat', () => {
     // If we receive a HMR request for our App container, then reload it using require (we can't do this dynamically with import)
-    const NextApp = require('./components/App').default;
+    const NextApp = require('./components/Chat').default;
     // And render it into the root element again
     render(
       <AppContainer>
-        <NextApp />
+        <Chat />
       </AppContainer>,
       rootEl
     );
--- src/server/server.ts
+++ src/server/server.ts
@@ -20,8 +20,23 @@ server.listen(3000);
 console.log('connected listening on port 3000');
 
 app.get('/', (req, res) => res.sendFile('index'));
-// when a client connects this fires
+
+
+// connects client to server socket 
 io.on('connection', (socket) => {
-  socket.emit('serverResponse', 'hello new player');
+  // sends a message to all clients A new player has joined
+  io.emit('A new player joined');
+  // when a the server recieves a message from the client execute callback
+  socket.on('newMessage', (data) => {
+    // sends an event 'userMessage' and the data form the callback to all clients
+    io.emit('userMessage', data);
+  });
+  // when a client disconnects
+  socket.on('disconnect', () => {
+    // sends a message to all clients
+    io.emit('user d/c')
+  });
 });
+
+
 

14c132bce7dfb82b6f330f3dd915ac1b4c5b7859
fuzzwizard <momojak@gmail.com>
2016-11-12 15:50:22 -0800

    Fix importation

--- src/client/game/index.ts
+++ src/client/game/index.ts
@@ -1,13 +1,28 @@
-/*eslint-disable func-style */
-import * as Phaser from 'phaser';
+/// <reference path="../../../node_modules/phaser-shim/phaser-shim.comments.d.ts" />
+ 
+import {Game, State} from 'phaser-shim';
 
-const game = new Phaser.Game(
-  800, 600, 'island', Phaser.AUTO, 
-  {preload, create, update}
-);
+class Boot extends State {
+  constructor() {
+    super();
+  }
 
-function preload() {};
+  init() {
+    this.stage.backgroundColor = '#EDEEC9'
+  }
+}
 
-function create() {};
+class BuilderGame extends Game {
+  constructor() {
+    super(800, 600, Phaser.AUTO, 'game');
+    this.state.add('Boot', Boot, false);
 
-function update() {};
+    this.state.start('Boot');
+  }
+
+
+}
+
+window.onload = () => {
+  const game = new BuilderGame();
+}

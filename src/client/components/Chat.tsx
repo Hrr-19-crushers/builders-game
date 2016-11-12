44d2a4df8c783e9477061fea05c618ab171bd608
David Bisrat <dabisrat@gmail.com>
2016-11-12 13:56:47 -0500

    added console.log to client and server to see user message

--- src/client/components/Chat.tsx
+++ src/client/components/Chat.tsx
@@ -1,46 +1,45 @@
 import * as React from 'react';
 import { postMessage } from '../socket_io';
 
-
-
-class Chat extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 'Please write an essay about '
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('An essay was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
+const Chat = () => {
     return (
-      <form onSubmit={this.handleSubmit}>
-        Name:
-        <textarea value={this.state.value} onChange={this.handleChange} />
-        <input type="submit" value="Submit" />
-      </form>
+        <div>
+            <ul id="messages"></ul>
+            <input className='message' />
+            <button onClick={() => postMessage('static test message')}>Send</button>
+        </div>
     );
-  }
-}
+};
 
-// const Chat = () => {
+// class Chat extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: 'Please write an essay about '
+//     };
+
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleChange(event) {
+//     this.setState({value: event.target.value});
+//   }
+
+//   handleSubmit(event) {
+//     alert('An essay was submitted: ' + this.state.value);
+//     event.preventDefault();
+//   }
+
+//   render() {
 //     return (
-//         <div>
-//             <ul id="messages"></ul>
-//             <input className='message' />
-//             <button onClick={() => postMessage(document.getElementsByClassName('message')[0].innerHTML)}>Send</button>
-//         </div>
+//       <form onSubmit={this.handleSubmit}>
+//         Name:
+//         <textarea value={this.state.value} onChange={this.handleChange} />
+//         <input type="submit" value="Submit" />
+//       </form>
 //     );
-// };
+//   }
+// }
+
 export default Chat;

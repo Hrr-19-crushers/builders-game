65c36457f5a423ea7a2236e63b5c7db5e018316b
jonathan.deng <dengjonathan@gmail.com>
2016-11-11 19:11:54 -0500

    react renders/ not finding correct web socket

--- src/client/socket_io.ts
+++ src/client/socket_io.ts
@@ -1,15 +1,21 @@
-import * as io from 'socket.io';
+import * as io from 'socket.io-client';
 
 // connect to server socket
-const socket: any = io('http:localhost');
+const socket: any = io.connect('http:localhost:3000');
 
 // this should be triggered in the function that sends the message 
-export const onClick = (message) =>{
-  socket.emit('newMessage', message);
+export const onClick = (message) => {
+    socket.emit('newMessage', message);
 };
 
-
-//
 socket.on('userMessage', (data) => {
-  // display data onto the dom 
+    // display data onto the dom 
+
+    // an event handler for 'someEvent'
+    socket.on('serverResponse', (data) => {
+        console.log(data);
+        // emits an event,'response', along with a some data
+        socket.emit('response', data);
+    });
 });
+export default socket;

30f634ab317e4982d31c812fa9a21e3610ef9508
jonathan.deng <dengjonathan@gmail.com>
2016-11-11 22:42:34 -0500

    server now serves client files

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,16 +1,17 @@
 import * as express from 'express';
 import * as http from 'http';
 import * as socket_io from 'socket.io';
+import * as path from 'path';
 
 const app = express();
 const server = http.createServer(app);
 const io = socket_io.listen(server);
 
-app.use(express.static(__dirname + '../../index.html'));
+app.use(express.static(path.join(__dirname, '../client')));
 server.listen(3000);
 console.log('connected listening on port 3000');
 
-app.get('/', (req, res) => res.send('200'));
+app.get('/', (req, res) => res.sendFile('index.html', { root: 'build/client/' }));
 
 // connects client to server socket 
 io.on('connection', (socket) => {

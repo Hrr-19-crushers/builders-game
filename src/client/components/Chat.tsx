01130915748022981f3f82bb67039d1c58a6bce8
David Bisrat <dabisrat@gmail.com>
2016-11-12 13:29:48 -0500

    made Chat statefull componenet

--- src/client/components/Chat.tsx
+++ src/client/components/Chat.tsx
@@ -1,13 +1,46 @@
 import * as React from 'react';
-import { onClick } from '../socket_io';
+import { postMessage } from '../socket_io';
 
-const Chat = () => {
+
+
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Please write an essay about '
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
     return (
-        <div>
-            <ul id="messages"></ul>
-            <input className='message' />
-            <button onClick={() => onClick(document.getElementsByClassName('message')[0].innerHTML)}>Send</button>
-        </div>
+      <form onSubmit={this.handleSubmit}>
+        Name:
+        <textarea value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
     );
-};
+  }
+}
+
+// const Chat = () => {
+//     return (
+//         <div>
+//             <ul id="messages"></ul>
+//             <input className='message' />
+//             <button onClick={() => postMessage(document.getElementsByClassName('message')[0].innerHTML)}>Send</button>
+//         </div>
+//     );
+// };
 export default Chat;

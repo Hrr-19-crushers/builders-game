b313119271c9d4a70f1de3b13f74332e040a00d7
jonathan.deng <dengjonathan@gmail.com>
2016-11-12 19:12:17 -0500

    working tests for chatReducers

--- test/client/chatReducers.test.ts
+++ test/client/chatReducers.test.ts
@@ -30,13 +30,13 @@ describe('chat reducer functions', () => {
       name: 'Silas',
       text: 'message',
       type: 'chat',
-      date: new Date()
+      date: new Date('January 1, 2016')
     },
     {
       name: 'Tom',
       text: 'new message',
       type: 'chat',
-      date: new Date()
+      date: new Date('March 1, 2016')
     }
   ];
   beforeEach(() => {
@@ -44,16 +44,22 @@ describe('chat reducer functions', () => {
   });
 
   it('can add a chat to the list', () => {
-    const nextState = chatReducer(INITIAL_STATE, { type: ADD_CHAT, payload: Chats[0] });
+    const nextState = chatReducer(INITIAL_STATE, addChatAction(Chats[0]));
     expect(nextState).to.eql({
       messages: [Chats[0]]
     });
   });
 
-  it('can clear old chats from the list', () => {
-    let state = chatReducer(INITIAL_STATE, { type: ADD_CHAT, payload: Chats[0] });
-    state = chatReducer(state, { type: ADD_CHAT, payload: Chats[1] });
-    const nextState = chatReducer(state, removeC({ name: 'Silas' }));
+  it('can clear all chats from list', () => {
+    const state = chatReducer(INITIAL_STATE, addChatAction(Chats[0]));
+    const nextState = chatReducer(INITIAL_STATE, clearChatsAction());
+    expect(nextState.messages).to.eql([]);
+  });
+
+  it('can clear chats before a certain date from the list', () => {
+    let state = chatReducer(INITIAL_STATE, addChatAction(Chats[0]));
+    state = chatReducer(state, addChatAction(Chats[1]));
+    const nextState = chatReducer(state, chatsAfterAction(new Date('Febuary 1, 2016')));
     expect(nextState.messages.length).to.equal(1);
     expect(nextState.messages[0].name).to.equal('Tom');
   });

c3b5f715df9697b1bc107d633b4944673cfadcac
David Bisrat <dabisrat@gmail.com>
2016-11-12 19:36:45 -0500

    deleted statefull react component

--- src/client/components/Chat.tsx
+++ src/client/components/Chat.tsx
@@ -10,36 +10,4 @@ const Chat = () => {
         </div>
     );
 };
-
-// class Chat extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       value: 'Please write an essay about '
-//     };
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   handleChange(event) {
-//     this.setState({value: event.target.value});
-//   }
-
-//   handleSubmit(event) {
-//     alert('An essay was submitted: ' + this.state.value);
-//     event.preventDefault();
-//   }
-
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         Name:
-//         <textarea value={this.state.value} onChange={this.handleChange} />
-//         <input type="submit" value="Submit" />
-//       </form>
-//     );
-//   }
-// }
-
 export default Chat;

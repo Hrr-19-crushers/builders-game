4daebd3a0449ec7ef1aec0f7a05ad1b17ec0e60d
David Bisrat <dabisrat@gmail.com>
2016-11-13 08:59:26 -0500

    added body parser middleware

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,14 @@
 import * as express from 'express';
 import * as socket_io from 'socket.io';
 import * as path from 'path';
+import * as bodyParser from 'body-parser';
 
 const app = express();
 const server = app.listen(3000, () => console.log('connected listening on port 3000'));
 const io = socket_io.listen(server);
 
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.sendFile('index.html', { root: 'build/client/' }));
 

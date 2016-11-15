71062bf86d51698d51e1c95b2fbade8c78b01c11
jonathan.deng <dengjonathan@gmail.com>
2016-11-14 19:35:13 -0500

    refactored to but input inside a form

--- src/client/components/Input.tsx
+++ src/client/components/Input.tsx
@@ -1,36 +1,36 @@
 import * as React from 'react';
 export default class Chat extends React.Component<any, any> {
-    constructor(props) {
-        super(props);
-        console.log(props);
-        this.state = {
-            input: ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      input: ''
+    };
+  }
 
-    public _onChange(event: any): void {
-        this.setState({ input: event.target.value });
-    }
+  public _onChange(event: any): void {
+    this.setState({ input: event.target.value });
+  }
 
-    public render() {
-        // const displays = this.props.messages.map(message => (<li>{message.name}says {message.text}</li>));
-        return (
-            <div>
-                <input id='input'
-                    value={this.state.input}
-                    onChange={e => this._onChange(e)}
-                    />
-                <button
-                    onClick={() => {
-                        this.props.addChat({
-                            text: this.state.input,
-                            user: 'David',
-                            date: JSON.stringify(new Date())
-                        });
-                        this.setState({ input: '' });
-                    } }
-                    >Send</button>
-            </div >
-        );
-    }
-};;
+  public render() {
+    return (
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          this.props.addChat({
+            text: this.state.input,
+            user: 'David',
+            date: JSON.stringify(new Date())
+          });
+          this.setState({ input: '' });
+        } }
+        >
+        <input id='input'
+          value={this.state.input}
+          onChange={e => this._onChange(e)}
+          />
+        <input type='submit' />
+      </form>
+    );
+  }
+};

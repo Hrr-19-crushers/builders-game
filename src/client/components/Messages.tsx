23f7b6f79680f292ef5455379593c84afee03236
jonathan.deng <dengjonathan@gmail.com>
2016-11-14 19:55:09 -0500

    updated to change classes of own or other's texts

--- src/client/components/Messages.tsx
+++ src/client/components/Messages.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import * as moment from 'moment';
 
-export default ({messages}) => {
+export default ({messages, user}) => {
   const items = messages.map(m =>
     (
-      <li key={m.date}>
+      <li key={m.date} className={user === m.user ? 'selfChat' : 'chat'}>
         <span className='chatName'>{m.user}: </span>
         <span className='chatText'>{m.text}  </span>
         <span className='chatDate'>{moment(JSON.parse(m.date)).fromNow()}</span>

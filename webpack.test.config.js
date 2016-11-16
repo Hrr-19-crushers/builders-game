d5676b281dc961b76f4fa3b2ebf992579cdfc1bf
jonathan.deng <dengjonathan@gmail.com>
2016-11-15 20:21:59 -0500

    karma testing now works

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -3,10 +3,10 @@ const path = require('path');
 const WebpackNotifierPlugin = require('webpack-notifier');
 
 module.exports = {
-  devtool: 'eval',
+  //devtool: 'eval',
   // This will be our app's entry point (webpack will look for it in the 'src' directory due to the modulesDirectory setting below). Feel free to change as desired.
   entry: [
-    'test'
+    'src/client/app.ts'
   ],
   // Output the bundled JS to dist/app.js
   output: {
@@ -20,25 +20,14 @@ module.exports = {
     modulesDirectories: ['src', 'node_modules'],
   },
   module: {
-    // lint all files before compile, transpile
-    preLoaders: [{
-      test: /\.js$/,
-      exclude: "node_modules",
-      loader: 'eslint-loader'
-    }],
     loaders: [
       // .ts(x) files should first pass through the Typescript loader, and then through babel
       {
         //match both ts and tsx
         test: /\.tsx?$/,
-        include: path.join(__dirname, 'src'),
-        loaders: ['ts-loader']
+        exclude: /node_modules/,
+        loaders: ['babel', 'ts-loader']
       }
     ]
-  },
-  plugins: [
-    new WebpackNotifierPlugin({
-      alwaysNotify: true
-    }),
-  ]
+  }
 };

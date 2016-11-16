5cb077a38914dfd2d0057a3acee08b8ee6613ea7
jonathan.deng <dengjonathan@gmail.com>
2016-11-15 19:17:29 -0500

    evened up the game and chat elements

--- src/client/game/index.ts
+++ src/client/game/index.ts
@@ -10,9 +10,9 @@ class Game extends Phaser.Game {
     let width = document.documentElement.clientWidth > 768
       ? 768
       : document.documentElement.clientWidth;
-    let height = document.documentElement.clientHeight > 1024
+    let height = document.documentElement.clientHeight > 1024 * 1.1 // give navbar some room
       ? 1024
-      : document.documentElement.clientHeight;
+      : document.documentElement.clientHeight * .9;
 
     super(width, height, Phaser.AUTO, 'content', null);
 

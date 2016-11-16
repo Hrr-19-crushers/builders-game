1df3edda6a976568be117249f2ef38f65018bc80
jonathan.deng <dengjonathan@gmail.com>
2016-11-15 19:51:00 -0500

    addded logic for mario to go last mushroom

--- src/client/game/index.ts
+++ src/client/game/index.ts
@@ -7,12 +7,12 @@ import { GameState } from './states/game';
 
 class Game extends Phaser.Game {
   constructor() {
-    let width = document.documentElement.clientWidth > 768
+    let width = document.documentElement.clientWidth > 768 * 1.4 // make room for chat
       ? 768
-      : document.documentElement.clientWidth;
+      : document.documentElement.clientWidth * 0.7;
     let height = document.documentElement.clientHeight > 1024 * 1.1 // give navbar some room
       ? 1024
-      : document.documentElement.clientHeight * .9;
+      : document.documentElement.clientHeight * .8;
 
     super(width, height, Phaser.AUTO, 'content', null);
 

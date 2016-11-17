fe698e2d574f21c27683de4eee4b2bbc06001032
jonathan.deng <dengjonathan@gmail.com>
2016-11-16 20:04:00 -0500

    fix (redux): fixme flag on echo bug

--- src/client/utils/serverActionMiddleware.ts
+++ src/client/utils/serverActionMiddleware.ts
@@ -1,5 +1,5 @@
 import { updateMessages } from './socket_io';
-
+// FIXME: causes echo on every update when multiple clients logged in
 export default store => next => action => {
   updateMessages(action);
   return next(action);

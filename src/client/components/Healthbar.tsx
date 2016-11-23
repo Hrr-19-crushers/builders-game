3dc7b12d47b4def57a27b35c3cc0f600facdf1c0
jonathan.deng <dengjonathan@gmail.com>
2016-11-23 15:55:32 -0500

    feat (badge): show user rank and name in user badge

--- src/client/components/Healthbar.tsx
+++ src/client/components/Healthbar.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 
 export default ({health}) => (
   <div className='healthBar'>
-    <div className='health healthLeft' style={{'width': health +'%'}}></div>
+    <div className='health healthLeft' style={{'width': health +'%'}}>
+      <span> Health: {health} </span>
+    </div>
     <div className='health healthLost' style={{'width': 100 - health +'%'}}></div>
   </div>
 );
--- src/client/components/UserBadge.tsx
+++ src/client/components/UserBadge.tsx
@@ -2,26 +2,26 @@ import * as React from 'react';
 
 import LoginContainer from '../containers/LoginContainer';
 
-export default ({profile}) => (
+export default({profile}) => (
   <div className="userBadge">
-    <img 
-      className="userBadgeProfile" 
-      src={profile.nickname ? profile.picture : '../../assets/sprites/mario.png'}>
-    </img>
-    <p className="userNickname">{profile.nickname ? profile.nickname: 'Guest'}</p>
-        <div id="boulder-badge">
-          <div className="badge">
-            <div className="part part-1"></div>
-            <div className="part part-2"></div>
-            <div className="part part-3"></div>
-            <div className="part part-4"></div>
-            <div className="part part-5"></div>
-            <div className="part part-6"></div>
-            <div className="part part-7"></div>
-            <div className="part part-8"></div>
-          </div>
-          <h1>Cascade badge</h1>
-        </div>
-    <LoginContainer />
+    <img
+      className="userBadgeProfile"
+      src={profile.nickname
+      ? profile.picture
+      : '../../assets/sprites/mario.png'}></img>
+    <div className='userHeader'>
+      <p className='userNickname'>{profile.nickname
+          ? profile.nickname
+          : 'Guest'}</p>
+      <p className='userRank'>{profile.nickname
+          ? 'Game Master'
+          : 'Cannon Fodder'}</p>
+    </div>
+    <div className='userBadges'>
+      <div className={profile.nickname ? 'loggedInBadge' : 'badge'}></div>
+      <div className={profile.nickname ? 'loggedInBadge' : 'badge'}></div>
+      <div className={profile.nickname ? 'loggedInBadge' : 'badge'}></div>
+    </div>
+    <LoginContainer/>
   </div>
 );

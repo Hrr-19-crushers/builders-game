8a02548ef2248dcd5182826927b14d1e86b971c3
Numose <andrew.cernek@gmail.com>
2016-11-23 14:53:53 -0700

    chore (heroku): compile site ready to deploy

---
+++ build/client/actions/actionInterface.js
@@ -0,0 +1,3 @@
+"use strict";
+;
+//# sourceMappingURL=actionInterface.js.map
---
+++ build/server/board.js
@@ -0,0 +1,73 @@
+"use strict";
+// import { Turn } from '../client/reducers/gameReducer';
+// import { turns } from './gameTurns';
+// import { testLayout } from './layouts';
+class Board {
+    constructor(layout) {
+        this.boardLayout = layout;
+    }
+    boardGetBoardState() {
+        return {
+            boardLayout: this.boardLayout
+        };
+    }
+    boardCharCanMoveDirection(direction, currentLocation) {
+        // heroku appears to not like destructuring yet
+        // const {x, y} = currentLocation;
+        const x = currentLocation.x;
+        const y = currentLocation.y;
+        const layout = this.boardLayout;
+        let can = false;
+        // don't let Nick S see this, he hates switch statements
+        switch (direction) {
+            case 'up':
+                if ((y - 1 in layout) && (layout[y - 1][x].p)) {
+                    can = true;
+                }
+                break;
+            case 'right':
+                if ((x + 1 in layout[y]) && (layout[y][x + 1].p)) {
+                    can = true;
+                }
+                break;
+            case 'down':
+                if ((y + 1 in layout) && (layout[y + 1][x].p)) {
+                    can = true;
+                }
+                break;
+            case 'left':
+                if ((x - 1 in layout[y]) && (layout[y][x - 1].p)) {
+                    can = true;
+                }
+                break;
+        }
+        return can;
+    }
+    // some duplication of code here and prob not necessary, but fine sep of concerns for now
+    boardGetNewCharLocation(direction, currentLocation) {
+        // there's probably a better way to destructure currentLocation here
+        // don't call this with a location unless you've already checked it's valid with boardCharCanMoveDirection
+        let newLocation;
+        switch (direction) {
+            case 'up':
+                newLocation = { x: currentLocation.x, y: currentLocation.y - 1 };
+                break;
+            case 'right':
+                newLocation = { x: currentLocation.x + 1, y: currentLocation.y };
+                break;
+            case 'down':
+                newLocation = { x: currentLocation.x, y: currentLocation.y + 1 };
+                break;
+            case 'left':
+                newLocation = { x: currentLocation.x - 1, y: currentLocation.y };
+                break;
+        }
+        return newLocation;
+    }
+    boardIsEnemyInTile(location) {
+        const tile = this.boardLayout[location.y][location.x];
+        return ('e' in tile && tile.e);
+    }
+}
+exports.Board = Board;
+//# sourceMappingURL=board.js.map

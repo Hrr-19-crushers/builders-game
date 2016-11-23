c0d9437f51c42a50d6295dc1763c43b63991579a
jonathan.deng <dengjonathan@gmail.com>
2016-11-23 14:44:53 -0500

    feat (style): show badges on user profile

--- src/client/components/UserBadge.tsx
+++ src/client/components/UserBadge.tsx
@@ -9,6 +9,19 @@ export default ({profile}) => (
       src={profile.nickname ? profile.picture : '../../assets/sprites/mario.png'}>
     </img>
     <p className="userNickname">{profile.nickname ? profile.nickname: 'Guest'}</p>
+        <div id="boulder-badge">
+          <div className="badge">
+            <div className="part part-1"></div>
+            <div className="part part-2"></div>
+            <div className="part part-3"></div>
+            <div className="part part-4"></div>
+            <div className="part part-5"></div>
+            <div className="part part-6"></div>
+            <div className="part part-7"></div>
+            <div className="part part-8"></div>
+          </div>
+          <h1>Cascade badge</h1>
+        </div>
     <LoginContainer />
   </div>
 );

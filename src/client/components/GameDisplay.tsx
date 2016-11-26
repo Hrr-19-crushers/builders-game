0c032f105dce735b9728e9cfb410bae785d0cd56
jonathan.deng <dengjonathan@gmail.com>
2016-11-26 16:14:23 -0500

    feat (display): update gamedisplay element

---
+++ src/client/components/GameDisplay.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import HealthbarContainer from '../containers/HealthbarContainer';
+
+export default () => (
+  <div className='gameDisplay'>
+    <HealthbarContainer />
+     are currently playing this game.
+    /8 treasure collected.
+  </div>
+)
--- src/client/components/Main.tsx
+++ src/client/components/Main.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 
 import Game from './Game';
 import Chat from './Chat';
+import GameDisplay from './GameDisplay';
 
 export default() => (
   <div className='Main'>
+    <GameDisplay />
     <Chat/>
     <Game/>
   </div>

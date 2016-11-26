e2a1dffd2d3cbfa60a6dc1bce97a9621b99f7062
David Bisrat <dabisrat@gmail.com>
2016-11-26 12:22:36 -0500

    chor (client): remove random console log.

--- src/client/components/UserMenu.tsx
+++ src/client/components/UserMenu.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
-export default ({user, isAuth}) => {
-  console.log(user);
+export default ({user}) => {
   return (<div className='menu'>
     <p className='menuName'>
       {user || `What\'s your name? Type '\\name [your name]' to set it`}

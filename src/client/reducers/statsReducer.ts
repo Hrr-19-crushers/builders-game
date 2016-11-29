55b482f533b42ecd4fe429487ecad04a9f9a4128
jonathan.deng <dengjonathan@gmail.com>
2016-11-28 20:04:59 -0500

    feat: (dashboard): update reducer to save client slices

--- src/client/reducers/statsReducer.ts
+++ src/client/reducers/statsReducer.ts
@@ -2,18 +2,28 @@
 import {Action} from '../actions/actionInterface';
 import {UPDATE_CLIENTS} from '../actions/actionTypes';
 
-export interface StatsState {
+export interface Slice {
+  date: number;
   clients: number;
 }
 
+export interface StatsState {
+  clients: number;
+  clientSlices: Slice[];
+}
+
 export const INITIAL_STATE: StatsState = {
-  clients: 0
+  clients: 0,
+  clientSlices: []
 };
 
 export const statsState = (state: StatsState = INITIAL_STATE, action: Action): StatsState => {
   switch (action.type) {
     case UPDATE_CLIENTS:
-      return Object.assign({}, state, {clients: action.payload}) ;
+      return Object.assign({}, state, {
+        clients: action.payload, 
+        clientSlices: state.clientSlices.concat({date: Date.now(), clients: action.payload})
+      });
     default:
       return state;
   }

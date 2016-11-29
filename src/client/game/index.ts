6743db86f62512c5482c4bfecfd3c84792964971
fuzzwizard <momojak@gmail.com>
2016-11-28 16:18:12 -0800

    fix (client): resize game canvas

--- src/client/game/index.ts
+++ src/client/game/index.ts
@@ -15,7 +15,7 @@ class Game extends Phaser.Game {
       ? 1024
       : document.documentElement.clientHeight * .8;
 
-    super(640, 480, Phaser.AUTO, 'game', null, false, false);
+    super(height, width, Phaser.AUTO, 'game', null, false, false);
 
     this.state.add('Boot', BootState, false);
     this.state.add('Splash', SplashState, false);

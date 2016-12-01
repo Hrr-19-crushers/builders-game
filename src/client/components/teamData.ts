ef8447e6baa0bb021219f03a8dd830e2e29a4a03
jonathan.deng <dengjonathan@gmail.com>
2016-12-01 16:52:23 -0500

    feat (about): finish styles for about page

--- src/client/components/teamData.ts
+++ src/client/components/teamData.ts
@@ -1,30 +1,29 @@
-export default [
-  {
-    name: 'Jon Deng',
-    avatar_url: 'https://avatars.githubusercontent.com/u/13730320?v=3',
-    html_url: 'https://github.com/dengjonathan',
-    title: 'Product Owner, Engineer',
-    likes: 'Redux, trail running, functional programming',
-  },
-  {
-    name: 'David Bisrat',
-    avatar_url: 'https://avatars.githubusercontent.com/u/20425875?v=3',
-    html_url: 'https://api.github.com/users/dabisrat',
-    title: 'Scrum Master, Engineer',
-    likes: ''
-  },
-  {
-    name: 'Mitch Small',
-    avatar_url: 'https://avatars.githubusercontent.com/u/12477375?v=3',
-    html_url: 'https://github.com/fuzzwizard',
-    title: 'Game Developer, Engineer',
-    likes: ''
-  },
-   {
-    name: 'Andrew Cernek',
-    avatar_url: 'https://avatars.githubusercontent.com/u/2206529?v=3',
-    html_url: 'https://api.github.com/users/Numose',
-    title: 'DevOps, Engineer',
-    likes: ''
-  }
-];
+export default [{
+  name: 'Jon Deng',
+  avatar_url: 'https://avatars.githubusercontent.com/u/13730320?v=3',
+  html_url: 'https://github.com/dengjonathan',
+  title: 'Product Owner, Engineer',
+  likes: 'Redux, trail running, functional programming',
+  city: 'Los Angeles'
+}, {
+  name: 'David Bisrat',
+  avatar_url: 'https://avatars.githubusercontent.com/u/20425875?v=3',
+  html_url: 'https://api.github.com/users/dabisrat',
+  title: 'Scrum Master, Engineer',
+  likes: 'Redux, trail running, functional programming',
+  city: 'Los Angeles'
+}, {
+  name: 'Mitch Small',
+  avatar_url: 'https://avatars.githubusercontent.com/u/12477375?v=3',
+  html_url: 'https://github.com/fuzzwizard',
+  title: 'Game Developer, Engineer',
+  likes: 'Redux, trail running, functional programming',
+  city: 'Los Angeles'
+}, {
+  name: 'Andrew Cernek',
+  avatar_url: 'https://avatars.githubusercontent.com/u/2206529?v=3',
+  html_url: 'https://api.github.com/users/Numose',
+  title: 'DevOps, Engineer',
+  likes: 'Redux, trail running, functional programming',
+  city: 'Los Angeles'
+}];

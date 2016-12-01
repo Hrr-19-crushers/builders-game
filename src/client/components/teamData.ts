c5b0821f2dc084f0c2e77ddc7fba1de64e0abf7f
jonathan.deng <dengjonathan@gmail.com>
2016-11-30 20:11:01 -0500

    feat (about): add about page

---
+++ src/client/components/teamData.ts
@@ -0,0 +1,28 @@
+export default = [
+  {
+    name: 'Jon Deng'
+    "avatar_url": "https://avatars.githubusercontent.com/u/13730320?v=3",
+    "html_url": "https://github.com/dengjonathan",
+    title: 'Product Owner, Engineer',
+    likes: 'Redux, trail running, functional programming',
+    text: ''
+  },
+  {
+    name: 'David Bisrat'
+    "avatar_url": "https://avatars.githubusercontent.com/u/20425875?v=3",
+    "html_url": "https://api.github.com/users/dabisrat",
+    title: 'Scrum Master, Engineer'
+  },
+  {
+    name: 'Mitch Small'
+    "https://avatars.githubusercontent.com/u/12477375?v=3",
+    "html_url": "https://github.com/fuzzwizard",
+    title: 'Game Developer, Engineer'
+  },
+   {
+    name: 'Andrew Cernek',
+    "https://avatars.githubusercontent.com/u/2206529?v=3",
+    "html_url": "https://api.github.com/users/Numose",
+    title: 'DevOps, Engineer'
+  }
+]

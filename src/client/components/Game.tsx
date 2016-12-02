8ba2dcf9238850deac0215b107df1880dab2ce16
jonathan.deng <dengjonathan@gmail.com>
2016-12-01 23:05:02 -0500

    yRevert "fix (game): allow game to render on navigate back from other views"
    This reverts commit 0a2b3d40a07b560ac18581aecca1247c616339b0.

--- src/client/components/Game.tsx
+++ src/client/components/Game.tsx
@@ -1,21 +1,3 @@
 import * as React from 'react';
 
-import {runGame} from '../game';
-import {getGameState} from '../store';
-
-export default class Game extends React.Component < any, any > {
-  componentDidMount() {
-    // if this is first render we don't want to render game until we get layout from server
-    if (getGameState().gameBoard) {
-      runGame();
-    }
-  }
-
-  render() {
-    return (
-      <div className='game' id='game'></div>
-    );
-  }
-}
-
-// export default () => (<div className='game' id='game'></div>);
+export default () => (<div className='game' id='game'></div>);

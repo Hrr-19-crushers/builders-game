e76859184a5d0b4966c0f4ec151a3698758bb258
jonathan.deng <dengjonathan@gmail.com>
2016-12-02 11:57:41 -0500

    refactor (store): fix typing issue when getting user state

--- src/client/store.ts
+++ src/client/store.ts
@@ -1,19 +1,23 @@
-import { createStore, applyMiddleware } from 'redux';
+import {createStore} from 'redux';
+
 import rootReducer from './reducers/rootReducer';
 import {GameState} from './reducers/gameReducer';
+import {UserState} from './reducers/userReducer';
 
-interface StoreState {
+export interface StoreState {
   gameState: Object;
   chatReducer: Object;
+  userState: Object;
 };
 
 const store = createStore(
   rootReducer
 );
 
-export const getGameState = (): GameState => {
-  const state = store.getState() as StoreState;
-  return state.gameState as GameState;
-};
- 
+const getState = (): StoreState => store.getState() as StoreState;
+
+export const getGameState = (): GameState => getState().gameState as GameState;
+
+export const getUserState = (): UserState => getState().userState as UserState;
+
 export default store;

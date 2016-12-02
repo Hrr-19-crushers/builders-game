86b0d6157276f299b02dbed36283da6d52df811c
David Bisrat <dabisrat@gmail.com>
2016-12-01 22:48:06 -0500

    fix (navbar): make it so logout button and github link don't overlap

--- src/client/utils/socket_io.ts
+++ src/client/utils/socket_io.ts
@@ -72,7 +72,7 @@ socket.on('gameState', gameState => {
 });
 
 socket.on('move', charState => {
-  console.log(charState);
+  //console.log(charState);
   store.dispatch(updateCharAction(charState));
 });
 
